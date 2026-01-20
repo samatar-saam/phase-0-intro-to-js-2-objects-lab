a89528ed8959e3899e8ec8c9a5a804720adaae85
samatar mohamed <samatarmohamed59@gmail.com>
2026-01-20 09:34:31 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+// helpers.js
+
+// Make employee available to the tests
+var employee = {};
+global.employee = employee;
+
+// Make functions available to the tests
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// expose them globally so Mocha can see them
+global.updateEmployeeWithKeyAndValue = updateEmployeeWithKeyAndValue;
+global.destructivelyUpdateEmployeeWithKeyAndValue = destructivelyUpdateEmployeeWithKeyAndValue;
+global.deleteFromEmployeeByKey = deleteFromEmployeeByKey;
+global.destructivelyDeleteFromEmployeeByKey = destructivelyDeleteFromEmployeeByKey;
